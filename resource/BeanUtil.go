bcaef69730179ff6b20dfbc1580ffbb15ebe3893
zhuxiujia <zhuxiujia@qq.com>
2018-11-05 16:15:19 +0800

    first commit

---
+++ resource/BeanUtil.go
@@ -0,0 +1,71 @@
+package resource
+
+import (
+	"reflect"
+	"log"
+)
+
+var DefaultResourceContext = make(map[string]interface{})
+
+func Register(beanName string,value interface{})  {
+	DefaultResourceContext[beanName]=value
+}
+
+
+//`bean:"***"`支持指针和实现interface的struct,指针（指针类型必须完全相同）struct(结构体可以完全一样，或者是继承实现某个接口的 struct)
+//扫描 给所有加了 `bean:"***"` 注解的成员 设置beanMap name关联的对象
+func AutoSetResourceProperty(beanMap map[string]interface{}, enableLog bool) {
+	for _, bean := range beanMap {
+		var v = reflect.ValueOf(bean)
+		if v.Kind() != reflect.Ptr {
+			panic("Bean: Bean must be a pointer")
+		}
+		v = v.Elem()
+		ScanAndSetProperty(v, beanMap, false)
+	}
+	//第二次遍历
+	for key, bean := range beanMap {
+		var v = reflect.ValueOf(bean)
+		if v.Kind() != reflect.Ptr {
+			panic("Bean: Bean must be a pointer")
+		}
+		v = v.Elem()
+		ScanAndSetProperty(v, beanMap, enableLog)
+		beanMap[key] = bean
+	}
+}
+
+func ScanAndSetProperty(v reflect.Value, beanMap map[string]interface{}, printInfo bool) (count int64) {
+	var t = v.Type()
+	if v.Kind() != reflect.Struct {
+		return count
+	}
+	for i := 0; i < t.NumField(); i++ {
+		var typeFieldItem = t.Field(i)
+		var beanName = typeFieldItem.Tag.Get("resource")
+		if beanName == "" {
+			continue
+		}
+		var field = v.Field(i)
+		var mapBeanInterface = beanMap[beanName]
+		if mapBeanInterface == nil {
+			if printInfo {
+				log.Println("[resource] property ", v.String(),".", beanName, " not register!")
+			}
+			continue
+		}
+		if typeFieldItem.Type.Kind() == reflect.Struct {
+			//loop scan bean feild
+			var c = ScanAndSetProperty(field, beanMap, printInfo)
+			count = count + c
+		}
+		var mapBeanValue = reflect.ValueOf(mapBeanInterface)
+		if mapBeanValue.Kind() != reflect.Ptr {
+			panic("[resource] property = " + v.String() +"."+ beanName + " must be a poiter")
+		}
+		mapBeanValue = mapBeanValue.Elem()
+		field.Set(mapBeanValue)
+		count++
+	}
+	return count
+}
---
+++ resource/BeanUtil_test.go
@@ -0,0 +1,52 @@
+package resource
+
+import (
+	"testing"
+	"fmt"
+)
+
+
+type B struct {
+	Name string
+}
+
+type A struct {
+	B        B  `resource:"b"`         //使用`resource:"b"` 注解注入对象 Annotated injection object
+	BPointer *B `resource:"b_pointer"` //使用`resource:"b"` 注解注入指针对象
+}
+
+//验证注解是否有效
+func (a A) Print() {
+	fmt.Println(a.B.Name)
+}
+//验证注解指针是否有效
+func (a A) PrintBPointer() {
+	fmt.Println(a.BPointer.Name)
+}
+
+
+
+func Test_Inject(t *testing.T) {
+	var b = B{
+		Name: "is B!",
+	}
+	var a A
+	var bAddress = &b
+
+
+	//You can also customize using your own context, as long as the context type meets the map[string]interface{}
+	//注册到context中,值必须为指向对象的指针类型
+	//你也可以自定义使用自己的context,只要context类型满足map[string]interface{}
+	Register("b", &b)
+	Register("a", &a)
+	Register("b_pointer", &bAddress)
+
+	//Automatically set all contain ` resource: "*" ` properties
+	//自动设置所有包含`resource:"*"`的属性
+	AutoSetResourceProperty(DefaultResourceContext, true)
+
+	//validation
+	//验证
+	a.Print()
+	a.PrintBPointer()
+}
